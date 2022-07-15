69307fde0d70c76de3ff4667f62b2637f5ff1be4
KimJinSeong-Git <acmiheee@gmail.com>
2022-07-15 16:35:04 +0900

    feat: lab routing

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("야호!")
+
+def create(request):
+    return HttpResponse('만들어!')
+
+def read(request, id):
+    return HttpResponse('나는: ' + id)
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,21 +1,15 @@
-"""myproject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# 사용자가 접속하는 Path에 따라 그 접속, 요청을 누가, 어떻게 처리할지 결정하는 파일( Routing )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+""" 시나리오
+ - http:/127.0.0.1/
+ - http:/127.0.0.1/app/
+
+ - http:/127.0.0.1/create/
+ - http:/127.0.0.1/read/1/
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('myapp.urls'))
 ]
