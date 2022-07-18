466844a95db8dc1be7cc38a28c5fcce0b03a7fe4
KimJinSeong-Git <acmiheee@gmail.com>
2022-07-18 17:03:11 +0900

    Feat Update

--- myapp/views.py
+++ myapp/views.py
@@ -19,6 +19,7 @@ def HTMLTemplate(articleTag, id=None):
                     <input type="submit" value="delete">
                 </form>
             </li>
+            <li><a href="/update/{id}">update</a></li>
         '''
     ol = ''
     for topic in topics:
@@ -88,3 +89,30 @@ def delete(request):
                 newTopics.append(topic)
         topics = newTopics
         return redirect('/')
+
+@csrf_exempt
+def update(request, id):
+    global topics
+    if request.method == 'GET':
+        for topic in topics:
+            if topic['id'] == int(id):
+                seletedTopic = {
+                    "title": topic['title'],
+                    "body": topic['body']
+                }
+        article = f'''
+            <form action="/update/{id}/" method="post">
+                <p><input type="text" name="title" placeholder="title" value={seletedTopic['title']}></p>
+                <p><textarea name="body" placeholder="body">{seletedTopic['body']}</textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article, id))
+    if request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        for topic in topics:
+            if topic['id'] == int(id):
+                topic['title'] = title
+                topic['body'] = body
+        return redirect(f'/read/{id}')
